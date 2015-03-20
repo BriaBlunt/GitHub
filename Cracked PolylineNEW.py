e779672aa2d55fadbd70377c6a31a34663620bd1
BriaBlunt <blunb635@newschool.edu>
2015-03-20 11:24:56 -0400

    3.20.15 Class

--- Cracked PolylineNEW.py
+++ Cracked PolylineNEW.py
@@ -4,9 +4,10 @@ import Rhino
 ## "import Rhino" imports lower level Rhino Common. It consist of a whole namespace of classes.
 ## Google Rhino Common SDK for more info
 
+
 ###CRACK Polygon
 
-def crackpolygon(polylines, count, maxGen):
+def crackpolygon(polylines, count, maxGen, attrPts):
     newPolylines = []
     if count > maxGen:
         return 
@@ -27,13 +28,43 @@ def crackpolygon(polylines, count, maxGen):
                 pts.append(rs.CurveEndPoint(crv))
                 pts.append(centroid[0])
                 pts.append(rs.CurveStartPoint(crv))
+                newpl = rs.AddPolyline(pts)
+                
+                addCurve = False
+                for pt in attrPts:
+                    if isPointInCurve(pt, newpl) :
+                        addCurve = True
+                    if addCurve :
+                        newPolyline.append(newpl)
+
                 p = rs.AddPolyline(pts)
                 newPolylines.append(p)
                 rs.DeleteObject(crv)
                 rs.DeleteObjects(centpt)
     return crackpolygon(newPolylines, count + 1, maxGen)
 
+
+def isPointInCurve (pt, curve):
+    curve = rs.coercecurve(curve)
+    rc = curve.Contains(pt)
+    
+    if rc == Rhino.Geometry.PointContainment
+       return False
+    else
+        return True
+        
+    return curve.Contains(pts)
+    endPt = rs.PointAdd(pt,[10000000, 0, 0])
+    tempLine = Rhino.Geometry.Curve(pt, endPt)
+    intercesctions = rs.CurveCurveIntersection (curve, tempLine)
+    if len(intersections)%2 == 0:
+        return false
+    else:
+        return true
+    
+
 def main():
+    attrPts = rs.GetPoints("select attractor Pts)
     maxGen = rs.GetInteger("How many iterations would you like to do?", 3)
     polyline = rs.GetCurveObject("pick a closed curve to crack")
     polylineGuid = polyline[0]
